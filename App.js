ff69db96081f79931bf2aa52972f71ac3d192b2b
jfcarocota <jfcarocota@gmail.com>
2020-09-21 12:37:32 -0700

    publish

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  state = {
+    userName: 'world',
+    displayName: 'world'
+  }
+
+  checkEmpty = name => name.length > 0 ? name : 'world';
+
+  catchName = name => this.setState({userName: this.checkEmpty(name)});
+
+  sendName = ()=> this.setState({displayName: this.state.userName});
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello {this.state.displayName}!</Text>
+        <TextInput placeholder='name' onChangeText={this.catchName}/>
+        <Button title='send name' onPress={this.sendName}/>
+        <StatusBar style="auto"/>
+      </View>    
+    );
+  }
 }
 
 const styles = StyleSheet.create({
