a16c09af6f86fe5484d220c60cd5beae28202c84
jfcarocota <jfcarocota@gmail.com>
2020-09-28 11:01:39 -0700

    vistas y cambio entre vistas

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component, Fragment } from 'react';
-import { Button, FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
-import HelloWorld from './components/HelloWorld';
-import axios from 'axios';
+import React, { Component } from 'react';
+import {StyleSheet, View } from 'react-native';
+import Routes from './components/Routes';
 
 export default class App extends Component {
 
@@ -10,76 +9,16 @@ export default class App extends Component {
     container: {
       flex: 1,
       backgroundColor: 'white',
-      alignItems: 'center',
       justifyContent: 'center',
-    },
-    list: {
-      
-    },
-    itemListContainer: {
-      marginLeft: 5
-    },
-    title:{
-      fontWeight: 'bold'
-    },
-    viewGrid: {
-      marginBottom: 30,
-      borderWidth: 1,
-      borderRadius: 3,
-      borderColor: 'lightgray',
-      borderBottomColor: 'white'
-    },
-    scroll: {
-      width: '95%'
-    },
-    viewTitle: {
-      color: 'black',
-      fontWeight: 'bold',
-      fontSize: 20,
-      paddingTop: 20
     }
   });
 
-  state = {
-    tacos: []
-  }
-
-  componentDidMount(){
-    axios.get('http://192.168.0.48:5000')
-    .then(res => res.data)
-    .then(data => {
-      this.setState({tacos: data})
-      console.log(this.state.tacos);
-    });
-  }
-
-  callOrder = ()=> {
-    console.log('taco ordenado')
-  }
-
-  renderTacoItem = taco =>{
-    return <View style={this.styles.viewGrid}>
-      <View style={this.styles.itemListContainer}>
-        <Text style={this.styles.title}>Nombre del taco</Text>
-        <Text>{taco.name}</Text>
-        <Text style={this.styles.title}>Cantidad de la orden</Text>
-        <Text>{taco.quantity} tacos</Text>
-        <Text style={this.styles.title}>¿Es picante?</Text>
-        <Text>{taco.pica}</Text>
-      </View>
-      <Button title='Ordenar' onPress={this.callOrder}/>
-    </View>;
-  } 
-
-  getTacosList = ()=> <FlatList style={this.styles.list} data={this.state.tacos} renderItem={({item}) => this.renderTacoItem(item)}/>;
+  
  
   render() {
     return (
       <View style={this.styles.container}>
-        <Text style={this.styles.viewTitle}>Menú</Text>
-        <ScrollView style={this.styles.scroll}> 
-          {this.getTacosList()}
-        </ScrollView>
+        <Routes/>
         <StatusBar style="auto"/>
       </View>    
     );
